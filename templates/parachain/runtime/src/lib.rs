f057c64f5cb8671387512e91edc2d892667c0a4b
rustlang-dev <rust.lang.developer@gmail.com>
2024-08-27 16:50:47 +0100

    declare Assets type for Runtime with index 12.

--- templates/parachain/runtime/src/lib.rs
+++ templates/parachain/runtime/src/lib.rs
@@ -277,6 +277,8 @@ mod runtime {
 	pub type Balances = pallet_balances;
 	#[runtime::pallet_index(11)]
 	pub type TransactionPayment = pallet_transaction_payment;
+	#[runtime::pallet_index(12)]
+	pub type Assets = pallet_assets;
 
 	// Governance
 	#[runtime::pallet_index(15)]
